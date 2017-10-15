15e3ddddb517a9ee23e8f786567c7012beddd79f
Sebastian Valdez <valdez.sebastian4@gmail.com>
2017-10-15 00:49:57 -0700

    Remove memoization in set_questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -31,7 +31,7 @@ class QuestionsController < ApplicationController
   private
 
   def set_question
-    @question ||= Question.find(params[:id])
+    @question = Question.find(params[:id])
   end
 
   def question_params

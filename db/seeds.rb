759e6f5f7bb6228bfe588892e942f15132775fe9
Sebastian Valdez <valdez.sebastian4@gmail.com>
2017-10-15 01:03:33 -0700

    Create users, qustion & answer

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,15 @@
-User.create! name: 'seb'
-User.create! name: 'tom'
+User.create!(name: 'seb')
+User.create!(name: 'tom')
+
+seb = User.find_by(name: 'seb')
+tom = User.find_by(name: 'tom')
+puts "Create 2 users \n"
+
+
+q = Question.create!(user_id: seb.id, name: 'What is the best programming language?')
+a = Answer.create!(question_id: q.id, name: 'Ruby')
+Response.create!(user_id: tom.id, answer_id: a.id)
+
+puts "Created a question with an Response"
+
+

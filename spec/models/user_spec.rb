98dd74808ed6a245d88bbafdcf41f5d523ef8fa4
Sebastian Valdez <valdez.sebastian4@gmail.com>
2017-10-15 00:42:06 -0700

    Create two tests for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,14 +2,16 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-	# should have a name
-	 before do 
-	 	@user = User.new
-	 end
+	describe 'Validates' do
+		let (:user_blank) { @user = User.new}
+		let(:user){User.new name: 'joe'}
 
-	describe 'Validates' do 
-		it 'name' do
-			expect(@user.valid?).to eq(false)
+		it 'name to not be true' do
+			expect(user_blank.valid?).to eq(false)
+		end
+
+		it 'name to be true' do 
+			expect(user.valid?).to eq(true)
 		end
 	end
 end

789df6e8eeeebc0f57ce2cbd4ac23ff5353c6840
Thomas Carli <thomas@procore.com>
2017-11-26 22:23:57 +0000

    production redis changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,6 +35,10 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.web_socket_server_url = "wss://quask-api.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['http://192.168.172.128:3001', 'http://localhost:3001']
+
+
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true

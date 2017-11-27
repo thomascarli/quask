dc417bae0bbca39963356d6ff661f103bc95413d
Thomas Carli <thomas@procore.com>
2017-11-27 06:16:33 +0000

    squawk

--- app/models/question.rb
+++ app/models/question.rb
@@ -18,16 +18,11 @@ class Question < ApplicationRecord
   has_many :answers
   accepts_nested_attributes_for :answers
 
-  def squawk
+  def self.squawk
     next_question = self.where(state: "on_deck").first
     on_deck_question = self.where(state: "pending").first
 
-    if next_question
-      next_question.broadcast
-    else
-      Question.create(user_id: 1, name: "0 Questions Bitch.").prepare.broadcast
-    end
-
+    next_question.broadcast if next_question
     on_deck_question.prepare if on_deck_question
   end
 

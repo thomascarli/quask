dfd648e35a9336fd8ed4abdff9a2b846c4d842d4
Thomas Carli <thomas@procore.com>
2017-12-06 06:56:40 +0000

    add codepen to header origin control

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,6 +29,9 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+  config.action_cable.url = "ws://192.168.172.128:3000/cable"
+  config.action_cable.allowed_request_origins = ['https://codepen.io', 'http://192.168.172.128:3001']
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
